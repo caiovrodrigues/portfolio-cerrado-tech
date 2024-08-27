b4479ae8366a5a79f7631907a1dcfba2a65c9891
Your Name <you@example.com>
2024-08-26 21:19:29 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-cerrado-tech';
+
+  indexSlideActive = 0;
+
+
+  avancarSlide(){
+    this.indexSlideActive++;
+    let sliders = document.querySelector(".sliders") as HTMLDivElement;
+    if(this.indexSlideActive <= 6){
+      sliders.style.marginLeft = `-${this.indexSlideActive}00%`;
+    }else{
+      sliders.style.marginLeft = `0%`;
+      this.indexSlideActive = 0;
+    }
+  }
+
+  voltarSlide(){
+    this.indexSlideActive--;
+    let sliders = document.querySelector(".sliders") as HTMLDivElement;
+    if(this.indexSlideActive >= 1){
+      sliders.style.marginLeft = `-${this.indexSlideActive}00%`;
+    }else{
+      sliders.style.marginLeft = `0%`;
+      this.indexSlideActive = 0;
+    }
+  }
 }
